330294045276d176317d10b56f1b6c76061e4388
Dustin McGilvray <dustinmcgilvray@gmail.com>
2019-04-25 14:36:32 -0500

    Landing Page has added styling.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,19 @@ import Layout from '../components/layout'
 import './index.css'
 
 const IndexPage = () => (
-  <div className="">
+  <div>
     <Layout>
-      <div className='card index-page-bg'>     
-        <div className="title has-text-white has-text-white-bold is-uppercase">Dustin McGilvray</div>
+      <div className="card">
+        <div className="left-craft">
+          <div id='word-craft'>CRAFT</div>
+        </div>
+        <div className="title has-text-white has-text-white-bold is-uppercase">
+          Dustin McGilvray
+        </div>
+        <div className="right-code">
+          <div id="word-code">CODE</div>
+        </div>
+        <div />
       </div>
     </Layout>
   </div>
