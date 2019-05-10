45286369b29c1af560db04e8f383a3b972ae9625
Dustin McGilvray <dustinmcgilvray@gmail.com>
2019-05-09 20:51:38 -0500

    More work on contact page layout.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,88 +7,134 @@ import './contact.css'
 const Contact = () => (
   <Layout>
     <div id="contact-all" className="card">
-        <div className="columns">
-          <div id="left-side" className="column is-half">
-            <section>
-              <div className="columns is-gapless">
-                <div className="column">
-                    <div className="card icon-card">
-                      <span className="icon">
-                        <a
-                          href="https://github.com/DustinMcGilvray"
-                          target="__blank"
-                        >
-                          <FontAwesomeIcon
-                            icon={['fab', 'github']}
-                            size="6x"
-                            color="red"
-                          />
-                        </a>
-                      </span>
-                    </div>
-                  </div>
-                <div className="column">
+      <div className="columns is-gapless">
+        <div id="left-side" className="column is-1">
+          <section>
+            <div className="columns is-gapless">
+              <div className="column">
                 <div className="card icon-card">
-                <span className="icon">
-                  <a
-                    href="https://github.com/DustinMcGilvray"
-                    target="__blank"
-                  >
-                    <FontAwesomeIcon
-                      icon={['fab', 'github']}
-                      size="6x"
-                      color="red"
-                    />
-                  </a>
-                </span>
-              </div>
+                  <span className="icon contact-icon">
+                    <a
+                      href="https://github.com/DustinMcGilvray"
+                      target="__blank"
+                    >
+                      <FontAwesomeIcon
+                        icon={['fab', 'github']}
+                        size="3x"
+                        color="red"
+                      />
+                    </a>
+                  </span>
                 </div>
               </div>
-              </section>
-              <section>
-              <div className="columns is-gapless">
-                <div className="column ">
-                <div className="card icon-card">
-                <span className="icon">
-                  <a
-                    href="https://github.com/DustinMcGilvray"
-                    target="__blank"
-                  >
-                    <FontAwesomeIcon
-                      icon={['fab', 'github']}
-                      size="6x"
-                      color="red"
-                    />
-                  </a>
-                </span>
-              </div>
-                </div>
-                <div className="column">
-                <div className="card icon-card">
-                <span className="icon">
-                  <a
-                    href="https://github.com/DustinMcGilvray"
-                    target="__blank"
-                  >
-                    <FontAwesomeIcon
-                      icon={['fab', 'github']}
-                      size="6x"
-                      color="red"
-                    />
-                  </a>
-                </span>
-              </div>
-                </div>
-              </div>
-            </section>
-          </div>
-          <div className="column is-half">
-            <div id="right-side">
-              <ContactForm />
             </div>
+          </section>
+
+          <section>
+            <div className="columns is-gapless">
+              <div className="column">
+                <div className="card icon-card">
+                  <span className="icon contact-icon">
+                    <a
+                      href="https://linkedin.com/in/dustinmcgilvray"
+                      target="__blank"
+                    >
+                      <FontAwesomeIcon
+                        icon={['fab', 'linkedin-in']}
+                        size="3x"
+                        color="red"
+                      />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <div className="columns is-gapless">
+              <div className="column ">
+                <div className="card icon-card">
+                  <span className="icon contact-icon">
+                    <a
+                      href="https://github.com/DustinMcGilvray"
+                      target="__blank"
+                    >
+                      <FontAwesomeIcon
+                        icon="comment-alt"
+                        size="3x"
+                        color="red"
+                      />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <div className="columns is-gapless">
+              <div className="column">
+                <div className="card icon-card">
+                  <span className="icon contact-icon">
+                    <a
+                      href="mailto:dustinmcgilvray@gmail.com?subject=Loved%20your%20Portfolio%20Site!"
+                      target="__blank"
+                    >
+                      <FontAwesomeIcon icon="envelope" size="3x" color="red" />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <div className="columns is-gapless">
+              <div className="column ">
+                <div className="card icon-card">
+                  <span className="icon contact-icon">
+                    <a
+                      href="https://github.com/DustinMcGilvray"
+                      target="__blank"
+                    >
+                      <FontAwesomeIcon icon="phone" size="3x" color="red" />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <div className="columns is-gapless">
+              <div className="column">
+                <div className="card icon-card">
+                  <span className="icon contact-icon">
+                    <a
+                      href="mailto:dustinmcgilvray@gmail.com?subject=Loved%20your%20Portfolio%20Site!"
+                      target="__blank"
+                    >
+                      <FontAwesomeIcon
+                        icon={['fab', 'skype']}
+                        size="3x"
+                        color="red"
+                      />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div className="column is-10">
+          <div id="right-side">
+            <ContactForm />
           </div>
         </div>
-        <div id="word-contact">Contact Me</div>
+        
+      </div>
     </div>
   </Layout>
 )
