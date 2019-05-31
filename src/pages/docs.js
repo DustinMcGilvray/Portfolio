559eccd4e5fa55050747071af9799b88aeac5ae4
Dustin McGilvray <dustinmcgilvray@gmail.com>
2019-05-31 12:10:39 -0500

    Working on Docs Hover Effect.

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Resume from "../files/dustin_mcgilvray_resume.pdf"
-import CoverLetter from "../files/dustin_mcgilvray_coverletter.pdf"
+import Resume from '../files/dustin_mcgilvray_resume.pdf'
+import CoverLetter from '../files/dustin_mcgilvray_coverletter.pdf'
 import './docs.css'
 
 const MyDocs = () => (
@@ -10,40 +10,55 @@ const MyDocs = () => (
       <section>
         <div className="columns is-gapless">
           <div className="column container-resume-pdf">
-            <div id="doc-resume-pdf" className="card document-card">
-              <h1 className="doc-card-resume-pdf">
-                <a href={Resume} target="_blank" rel="noopener noreferrer">
-                  Download My Resume
-                </a>
-              </h1>
-              <div class="overlay-resume-pdf" />
-            </div>
+            <figure className="document-card effect-oscar">
+              <img src={require('../images/file_vault_bw.png')} alt="" />
+              <a href={Resume} target="_blank" rel="noopener noreferrer">
+                <figcaption>
+                  <h2>
+                    PDF<span>Resume</span>
+                  </h2>
+                  <p>Take a moment to review my credentials!</p>
+                </figcaption>
+              </a>
+            </figure>
           </div>
         </div>
       </section>
       <section>
         <div className="columns is-gapless">
           <div className="column container-resume-npm">
-            <div id="doc-resume-pdf" className="card document-card">
-              <h1 className="doc-card-resume-npm">
-                  npm resume
-              </h1>
-              <div class="overlay-resume-pdf" />
-            </div>
+            <figure className="document-card effect-oscar">
+              <img src={require('../images/file_vault_bw.png')} alt="" />
+              <a
+                href="https://www.npmjs.com/package/dustinmcgilvray_resume"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <figcaption>
+                  <h2>
+                    NPM<span>Resume</span>
+                  </h2>
+                  <p>Here is Resume as a CLI App Packaged as an NPM Module!</p>
+                </figcaption>
+              </a>
+            </figure>
           </div>
         </div>
       </section>
       <section>
         <div className="columns is-gapless">
           <div className="column container-coverletter-pdf">
-            <div id="doc-coverletter-pdf" className="card document-card">
-              <h1 className="doc-card-resume-pdf">
-                <a href={CoverLetter} target="_blank" rel="noopener noreferrer">
-                  Cover Letter PDF
-                </a>
-              </h1>
-              <div class="overlay-coverletter-pdf" />
-            </div>
+            <figure className="document-card effect-oscar">
+              <img src={require('../images/file_vault_bw.png')} alt="" />
+              <a href={CoverLetter} target="_blank" rel="noopener noreferrer">
+                <figcaption>
+                  <h2>
+                    PDF<span>Cover Letter</span>
+                  </h2>
+                  <p>Take a Moment to Read my Cover Letter!</p>
+                </figcaption>
+              </a>
+            </figure>
           </div>
         </div>
       </section>
