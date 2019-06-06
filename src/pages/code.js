f6c12be6c21243e6b16e9230cb0d5387ac299bd2
Dustin McGilvray <dustinmcgilvray@gmail.com>
2019-06-05 21:57:50 -0500

    Code Page Icons Done.

--- src/pages/code.js
+++ src/pages/code.js
@@ -5,12 +5,12 @@ import './code.css'
 const MyCode = () => (
   <Layout>
     <div id="code-card" className="card">
-      <div className="columns">
+      <div className="columns is-gapless">
         <div className="column">
           <h1>Code Summary</h1>
           <p>Place some verbage here about coding and stuff.</p>
         </div>
-        <div className="column">
+        <div id="skills-side" className="column">
           <section>
             <div className="columns is-gapless">
               <div className="column">
@@ -27,7 +27,7 @@ const MyCode = () => (
               <div className="column">
                 <div id="css-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
+                    <div class="skill-label">
                       <div>CSS3</div>
                     </div>
                     <div className="skill-graph">Skill Level Graph</div>
@@ -37,13 +37,33 @@ const MyCode = () => (
               <div className="column">
                 <div id="javascript-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
+                    <div class="skill-label">
                       <div>JavaScript</div>
                     </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
+              <div className="column">
+              <div id="bootstrap-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Bootstrap</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div id="firebase-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Firebase</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
             </div>
           </section>
 
@@ -51,19 +71,18 @@ const MyCode = () => (
             <div className="columns is-gapless">
               <div className="column">
                 <div id="jquery-skill" className="logo-card">
-                <div className="logo-overlay">
-                <div class="skill-label">
+                  <div className="logo-overlay">
+                    <div class="skill-label">
                       <div>jQuery</div>
                     </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
-
               <div className="column">
                 <div id="ajax-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
+                    <div class="skill-label">
                       <div>AJAX</div>
                     </div>
                     <div className="skill-graph">Skill Level Graph</div>
@@ -73,13 +92,33 @@ const MyCode = () => (
               <div className="column">
                 <div id="node-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
+                    <div class="skill-label">
                       <div>Node.js</div>
                     </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
+              <div className="column">
+              <div id="express-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Express</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div id="handlebars-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Handlebars</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
             </div>
           </section>
 
@@ -88,34 +127,53 @@ const MyCode = () => (
               <div className="column">
                 <div id="rest-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
+                    <div class="skill-label">
                       <div>API</div>
                     </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
-
               <div className="column">
                 <div id="postman-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
-                  <div>Postman</div>
-                </div>
+                    <div class="skill-label">
+                      <div>Postman</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
               <div className="column">
-                <div id="swagger-skill" className="logo-card">
-                  <div className="logo-overlay">
+              <div id="swagger-skill" className="logo-card">
+                <div className="logo-overlay">
                   <div class="skill-label">
-                  <div>Swagger.io</div>
-                </div>
-                    <div className="skill-graph">Skill Level Graph</div>
+                    <div>Swagger.io</div>
                   </div>
+                  <div className="skill-graph">Skill Level Graph</div>
                 </div>
               </div>
+            </div>
+              <div className="column">
+              <div id="heroku-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Heroku</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div id="materialize-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Materialize</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
             </div>
           </section>
 
@@ -124,20 +182,19 @@ const MyCode = () => (
               <div className="column">
                 <div id="mysql-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
-                  <div>mySQL</div>
-                </div>
+                    <div class="skill-label">
+                      <div>mySQL</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
-
               <div className="column">
                 <div id="sequelize-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
-                  <div>Sequelize</div>
-                </div>
+                    <div class="skill-label">
+                      <div>Sequelize</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
@@ -145,13 +202,33 @@ const MyCode = () => (
               <div className="column">
                 <div id="mongodb-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
-                  <div>MongoDB</div>
-                </div>
+                    <div class="skill-label">
+                      <div>MongoDB</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
+              <div className="column">
+              <div id="mocha-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Mocha</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div id="chai-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Chai</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
             </div>
           </section>
 
@@ -160,20 +237,19 @@ const MyCode = () => (
               <div className="column">
                 <div id="react-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
-                  <div>React</div>
-                </div>
+                    <div class="skill-label">
+                      <div>React</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
-
               <div className="column">
                 <div id="router-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
-                  <div>React Router</div>
-                </div>
+                    <div class="skill-label">
+                      <div>React Router</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
@@ -181,42 +257,146 @@ const MyCode = () => (
               <div className="column">
                 <div id="es6-skill" className="logo-card">
                   <div className="logo-overlay">
-                  <div class="skill-label">
-                  <div>ES6</div>
-                </div>
+                    <div class="skill-label">
+                      <div>ES6</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
+              <div className="column">
+              <div id="react-materialize-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>React Materialize</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div id="bulma-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Bulma</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
             </div>
           </section>
 
           <section>
             <div className="columns is-gapless">
               <div className="column">
-                <div id="html-skill" className="logo-card">
+                <div id="gatsby-skill" className="logo-card">
                   <div className="logo-overlay">
+                    <div class="skill-label">
+                      <div>Gatsby.js</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
+              <div className="column">
+                <div id="graphql-skill" className="logo-card">
+                  <div className="logo-overlay">
+                    <div class="skill-label">
+                      <div>GraphQL</div>
+                    </div>
+                    <div className="skill-graph">Skill Level Graph</div>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div id="contentful-skill" className="logo-card">
+                  <div className="logo-overlay">
+                    <div class="skill-label">
+                      <div>Contentful</div>
+                    </div>
+                    <div className="skill-graph">Skill Level Graph</div>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+              <div id="styled-components-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Styled Components</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div id="netlify-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>Netlify</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
+            </div>
+          </section>
 
+          <section>
+            <div className="columns is-gapless">
               <div className="column">
-                <div id="html-skill" className="logo-card">
+                <div id="git-skill" className="logo-card">
                   <div className="logo-overlay">
+                    <div class="skill-label">
+                      <div>Git</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
               <div className="column">
-                <div id="html-skill" className="logo-card">
+                <div id="github-skill" className="logo-card">
                   <div className="logo-overlay">
+                    <div class="skill-label">
+                      <div>GitHub</div>
+                    </div>
+                    <div className="skill-graph">Skill Level Graph</div>
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+              <div id="gitlab-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>GitLab</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div id="vscode-skill" className="logo-card">
+                <div className="logo-overlay">
+                  <div class="skill-label">
+                    <div>VS Code</div>
+                  </div>
+                  <div className="skill-graph">Skill Level Graph</div>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+                <div id="terminal-skill" className="logo-card">
+                  <div className="logo-overlay">
+                    <div class="skill-label">
+                      <div>Terminal</div>
+                    </div>
                     <div className="skill-graph">Skill Level Graph</div>
                   </div>
                 </div>
               </div>
             </div>
           </section>
+
         </div>
       </div>
     </div>
