955aba04287db3702f5efa9d7cc0e9059c5f9584
Dustin McGilvray <dustinmcgilvray@gmail.com>
2019-08-11 11:46:32 -0500

    Added responsiveness to contactpage. Added onClicks to contact summary and contact form.

--- src/components/contact-summary.js
+++ src/components/contact-summary.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './contact-summary.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const ContactSummary = () => (
+const ContactSummary = (props) => (
   <div id="contact-summary-card" className="card is-shadowless">
     <div className="card-content">
       <h1 className="title is-centered has-text-white">
@@ -14,42 +14,88 @@ const ContactSummary = () => (
       <ul>
         <li className="has-text-white">
           <span className="icon contact-icon">
-            <FontAwesomeIcon icon="phone" color="white" size="lg" />
+            <a href="tel:254-640-2904">
+              <FontAwesomeIcon
+                icon="phone"
+                color="white"
+                size="lg"
+                className="is-hidden-mobile"
+              />
+              <FontAwesomeIcon
+                icon="phone"
+                color="white"
+                size="2x"
+                className="is-hidden-desktop"
+              />
+            </a>
           </span>
-          254.640.2904
+          <span className="is-hidden-mobile">254.640.2904</span>
         </li>
-      </ul>
-      <ul>
         <li className="has-text-white">
           <span className="icon contact-icon">
-            <FontAwesomeIcon icon="envelope" color="white" size="lg" />
+          <a href="mailto:dustinmcgilvray@gmail.com?subject=Loved%20Your%20Portfolio&" target="_top">
+            <FontAwesomeIcon
+              icon="envelope"
+              color="white"
+              size="lg"
+              className="is-hidden-mobile"
+            />
+            <FontAwesomeIcon
+              icon="envelope"
+              color="white"
+              size="2x"
+              className="is-hidden-desktop"
+            />
+            </a>
           </span>
-          dustinmcgilvray@gmail.com
+          <span className="is-hidden-mobile">dustinmcgilvray@gmail.com</span>
+        </li>
+        <li className="has-text-white">
+          <span className="icon contact-icon">
+            <a href="https://github.com/DustinMcGilvray" target="__blank">
+              <FontAwesomeIcon
+                icon={['fab', 'github']}
+                color="white"
+                size="lg"
+                className="is-hidden-mobile"
+              />
+              <FontAwesomeIcon
+                icon={['fab', 'github']}
+                color="white"
+                size="2x"
+                className="is-hidden-desktop"
+              />
+            </a>
+          </span>
+          <span className="is-hidden-mobile">DustinMcGilvray</span>
+        </li>
+        <li className="has-text-white">
+          <span className="icon contact-icon">
+            <a href="https://linkedin.com/in/dustinmcgilvray" target="__blank">
+              <FontAwesomeIcon
+                icon={['fab', 'linkedin']}
+                color="white"
+                size="lg"
+                className="is-hidden-mobile"
+              />
+              <FontAwesomeIcon
+                icon={['fab', 'linkedin']}
+                color="white"
+                size="2x"
+                className="is-hidden-desktop"
+              />
+            </a>
+          </span>
+          <span className="is-hidden-mobile">dustinmcgilvray</span>
         </li>
       </ul>
     </div>
-    <footer id="contact-summary-footer" className="card-footer">
-      <a
-        href="https://github.com/DustinMcGilvray"
-        className="card-footer-item"
-        target="__blank"
-      >
-        <FontAwesomeIcon icon={['fab', 'github']} color="white" size="2x" />
-      </a>
-      <a
-        href="https://linkedin.com/in/dustinmcgilvray"
-        className="card-footer-item"
-        target="__blank"
-      >
-        <FontAwesomeIcon icon={['fab', 'linkedin']} color="white" size="2x" />
-      </a>
-      <a
-        href="https://codepen.io/dustinmcgilvray/"
-        className="card-footer-item"
-        target="__blank"
-      >
-        <FontAwesomeIcon icon={['fab', 'codepen']} color="white" size="2x" />
-      </a>
+    <footer id="contact-summary-footer" className="card-footer is-hidden-desktop">
+      <span className="card-footer-item">
+        <button className="button is-outlined is-fullwidth" onClick={props.showContactForm}>
+          Contact Form
+        </button>
+      </span>
     </footer>
   </div>
 )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,21 +5,48 @@ import ContactForm from '../components/contact-form'
 import './contact.css'
 
 class Contact extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isContactInfoHidden: false,
+      isContactFormHidden: true,
+    }
+  }
+
+  showContactForm = () => {
+    this.setState({
+      ...this.state,
+      isContactInfoHidden: true,
+      isContactFormHidden: false,
+    })
+  }
+
+  closeContactForm = () => {
+    this.setState({
+      ...this.state,
+      isContactInfoHidden: false,
+      isContactFormHidden: true,
+    })
+  }
 
   render() {
     return (
       <Layout>
         <div id="contact-card" className="card is-shadowless">
-          <div className="columns is-gapless">
-            <div className="column">
-              <ContactSummary/>
-              </div>
-            <div className="column">
-            <div className="columns is-gapless">
-            <div className="column">
-              <ContactForm/>
-              </div>
-              </div>
+          <div className="columns is-gapless ">
+            <div className="column is-hidden-mobile">
+              <ContactSummary />
+            </div>
+            <div className="column is-hidden-desktop">
+              {this.state.isContactFormHidden && (
+                <ContactSummary showContactForm={this.showContactForm} />
+              )}
+              {this.state.isContactInfoHidden && (
+                <ContactForm closeContactForm={this.closeContactForm} />
+              )}
+            </div>
+            <div className="column is-hidden-mobile">
+              <ContactForm />
             </div>
           </div>
         </div>
